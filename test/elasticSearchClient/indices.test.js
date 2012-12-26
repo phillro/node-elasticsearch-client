efd61626eebe0f16772e835f0d581ffc0818dbad
Juzer Ali <er.juzerali@gmail.com>
2012-12-27 02:47:28 +0530

    Added file .gitignore
    Added node_modules to .npmignore
    
    Added mocha and chai to devDependencies
    
    Rewrote core tests using mocha, chai
    
    Rewrite cluster tests using mocha chai
    
    Rewrote indices test using mocha chai

--- test/elasticSearchClient/indices.test.js
+++ test/elasticSearchClient/indices.test.js
@@ -1,6 +1,7 @@
-assert = require('assert');
-ElasticSearchClient = require('../../lib/elasticsearchclient/elasticSearchClient.js');
-var hashlib = require('hashlib')
+var ElasticSearchClient = require('../../lib/elasticsearchclient/elasticSearchClient.js')
+,   mocha = require("mocha")
+,   should = require("chai").should();
+
 var serverOptions = {
     host: 'localhost',
     port: 9200,
@@ -13,235 +14,288 @@ var serverOptions = {
 
 
 var indexName = 'your_index_name';
-var objName = 'your_object_name'
+var objName = 'your_object_name';
 
 var elasticSearchClient = new ElasticSearchClient(serverOptions);
 
 
 var tweet = {"tweet" : {
-    "properties" : {
-        "message" : {"type" : "string", "store" : "yes"}
-    }
-}
-}
-
-
-testAliases = function() {
-    var aliases = {
-        "actions":
-                [
-                    { "add" : { "index" : "test1", "alias" : "alias1" } }
-                ]
-    }
-    elasticSearchClient.createIndex(indexName, aliases)
-            .on('data', function(data) {
-                assert.ok(JSON.parse(data).ok, "testAliases failed")
-            })
-            .exec()
-
-}
-
-testAnalyze = function() {
-    elasticSearchClient.analyze(indexName,'this is a test')
-            .on('data', function(data) {
-                console.log(data)
-                //assert.ok(JSON.parse(data).ok, "testAnalyze failed")
-            })
-            .exec()
-}
-
-testCreateIndex = function() {
-    elasticSearchClient.createIndex(indexName)
-            .on('data', function(data) {
-            console.log(data);
-                //assert.ok(JSON.parse(data))
-            })
-            .exec()
-}
-
-testDeleteIndex = function() {
-    elasticSearchClient.deleteIndex(indexName)
-            .on('data', function(data) {
-                assert.ok(JSON.parse(data))
-            })
-            .exec()
-}
-
-testOpenIndex = function() {
-    elasticSearchClient.openIndex(indexName)
-            .on('data', function(data) {
-                assert.ok(JSON.parse(data).ok, 'testOpenIndex failed')
-            })
-            .exec()
-
-}
-
-testCloseIndex = function() {
-    elasticSearchClient.openIndex(indexName)
-            .on('data', function(data) {
-                assert.ok(JSON.parse(data).ok, 'testCloseIndex failed')
-            })
-            .exec()
-
-}
-
-testGetSettings = function() {
-    elasticSearchClient.getSettings(indexName)
-            .on('data', function(data) {
-                console.log(data);
-                assert.ok(JSON.parse(data))
-            })
-            .exec()
-}
-
-testUpdateSettings = function() {
-    var settings = {
-        "index" : {
-            "number_of_replicas" : 4
+        "properties" : {
+            "message" : {"type" : "string", "store" : "yes"}
         }
     }
-    elasticSearchClient.updateSettings(indexName, settings)
-            .on('data', function(data) {
-                //assert.ok(JSON.parse(data), 'testSnapshot failed')
-                assert.ok(JSON.parse(data).ok, 'testUpdateSettings failed')
+};
 
-            })
-            .exec()
-}
-
-testGetMapping = function() {
-    elasticSearchClient.getMapping(indexName, objName)
-            .on('data', function(data) {
-            console.log(data);
-                assert.ok(JSON.parse(data), 'testGetMapping failed')
-            })
-            .exec()
-}
-
-testPutMapping = function() {
-    elasticSearchClient.putMapping(indexName, objName, tweet)
-            .on('data',
-            function(data) {
-                assert.ok(JSON.parse(data), 'testPutMapping failed')
-            }).on('done', function(data) {
-            })
-            .exec()
-}
-
-testDeleteMapping = function() {
-    elasticSearchClient.deleteMapping(indexName, objName)
-            .on('data', function(data) {
-                assert.ok(JSON.parse(data), 'testDeleteMapping failed')
-            })
-            .exec()
-
-}
-
-testRefresh = function() {
-    elasticSearchClient.refresh(indexName)
-            .on('data', function(data) {
-                console.log(data)
-                assert.ok(JSON.parse(data), 'testRefresh failed')
-            })
-            .exec()
-}
-
-testOptimize = function() {
-
-    elasticSearchClient.optimize(indexName)
-            .on('data', function(data) {
-                console.log(data)
-                assert.ok(JSON.parse(data), 'testOptimize failed')
-            })
-            .exec()
-}
-
-testFlush = function() {
-    elasticSearchClient.flush(indexName)
-            .on('data', function(data) {
-                console.log(data)
-                assert.ok(JSON.parse(data), 'testFlush failed')
-            })
-            .exec()
-}
-
-testSnapShot = function() {
-
-    elasticSearchClient.snapshot(indexName)
-            .on('data', function(data) {
-                //assert.ok(JSON.parse(data), 'testSnapshot failed')
-                console.log(data)
-            })
-            .exec()
-}
+var template = {
+                "template" : "te*",
+                "settings" : {
+                    "number_of_shards" : 1
+                }
+            };
 
 
-testDefineTemplate = function() {
-    var template = {
-        "template" : "te*",
-        "settings" : {
-            "number_of_shards" : 1
-        }
-    }
-    elasticSearchClient.defineTemplate('testTemplate', template)
-            .on('data', function(data) {
-                //assert.ok(JSON.parse(data), 'testDefineTemplate failed')
-                console.log(data)
-            })
-            .exec()
-}
-
-testGetTemplate = function() {
-    elasticSearchClient.getTemplate('testTemplate', template)
-            .on('data', function(data) {
-                //assert.ok(JSON.parse(data), 'testGetTemplate failed')
-                console.log(data)
-            })
-            .exec()
-}
-
-testDeleteTemplate = function() {
-    elasticSearchClient.deleteTemplate('testTemplate', template)
-            .on('data', function(data) {
-                //assert.ok(JSON.parse(data), 'testDeleteTemplate failed')
-                console.log(data)
-            })
-            .exec()
-}
-
-testStatus = function() {
-    elasticSearchClient.status(indexName)
-            .on('data', function(data) {
-                //console.log(data)
-                assert.ok(JSON.parse(data), 'testStatus failed')
-            })
-            .exec()
-}
-
-testClearCache = function() {
-    elasticSearchClient.clearCache(indexName)
-            .on('data', function(data) {
-                assert.ok(JSON.parse(data), 'testClearCache failed')
-            })
-            .exec()
-}
-
-testGetMapping();
-
-testCreateIndex()
-testPutMapping();
-testDeleteMapping();
-testRefresh();
-testOptimize();
-testFlush();
-testSnapShot();
-testUpdateSettings();
-testStatus();
-testClearCache();
-testCloseIndex();
-testOpenIndex()
-testAnalyze();
-testDeleteIndex();
+describe("ElasticSearchClient indices api", function(){
+    describe("Aliases", function(){
+        it("should alias", function(done){
+            var aliases = {
+                "actions":
+                        [
+                            { "add" : { "index" : "1", "alias" : "alias1" } }
+                        ]
+            }
+            elasticSearchClient.createIndex(indexName, aliases)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
 
 
 
+    describe("#analyze", function(){
+        it("should analyze", function(done){
+            elasticSearchClient.analyze(indexName,'this is a ')
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#createIndex", function(){
+        it("should create an index", function(done){
+            elasticSearchClient.createIndex(indexName)
+                .on('data', function(data) {
+                    console.log(data);
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#deleteIndex", function(){
+        it("should delete index", function(done){
+            elasticSearchClient.deleteIndex(indexName)
+                .on('data', function(data) {
+                     JSON.parse(data).should.be.ok;
+                     done()
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#openIndex", function(){
+        it("should open the given index", function(done){
+            elasticSearchClient.openIndex(indexName)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#closeIndex", function(){
+        it("should close the given index", function(done){
+            elasticSearchClient.openIndex(indexName)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#getSettings", function(){
+    	it("should get settings", function(done){
+            elasticSearchClient.getSettings(indexName)
+                .on('data', function(data) {
+                    console.log(data);
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#updateSettings", function(){
+    	it("should update settings", function(done){
+            var settings = {
+                "index" : {
+                    "number_of_replicas" : 4
+                }
+            }
+            elasticSearchClient.updateSettings(indexName, settings)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#getMapping", function(){
+    	it("should get mappings", function(done){
+            elasticSearchClient.getMapping(indexName, objName)
+                .on('data', function(data) {
+                    console.log(data);
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#putMapping", function(){
+    	it("should put mappings", function(done){
+            elasticSearchClient.putMapping(indexName, objName, tweet)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+                }).on('done', function(data) {
+
+                })
+                .exec();
+        });
+    });
+
+
+    describe("deleteMapping#", function(){
+    	it("should delete mappings", function(done){
+            elasticSearchClient.deleteMapping(indexName, objName)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#refresh", function(){
+    	it("should refresh", function(done){
+            elasticSearchClient.refresh(indexName)
+                    .on('data', function(data) {
+                        JSON.parse(data).should.be.ok;
+                        done();
+                    })
+                    .exec();
+        });
+    });
+
+
+    describe("#optimize", function(){
+    	it("should optimize", function(done){
+            elasticSearchClient.optimize(indexName)
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#flush", function(){
+    	it("should flush", function(done){
+            elasticSearchClient.flush(indexName)
+                .on('data', function(data) {
+                    console.log(data);
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#snapShot", function(){
+    	it("should take a snapshot", function(done){
+            elasticSearchClient.snapshot(indexName)
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+    describe("#defineTemplate", function(){
+    	it("should define a template", function(done){
+            elasticSearchClient.defineTemplate('Template', template)
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#getTemplate", function(){
+        	it("should get a template", function(done){
+            elasticSearchClient.getTemplate('Template', template)
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#deleteTemplate", function(){
+    	it("should delete a template", function(done){
+            elasticSearchClient.deleteTemplate('Template', template)
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#status", function(){
+    	it("should provide status", function(done){
+            elasticSearchClient.status(indexName)
+                .on('data', function(data) {
+                    console.log(data)
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+
+
+    describe("#clearCache", function(){
+    	it("should clear the cache", function(done){
+            elasticSearchClient.clearCache(indexName)
+                .on('data', function(data) {
+                    JSON.parse(data).should.be.ok;
+                    done();
+                })
+                .exec();
+        });
+    });
+});
